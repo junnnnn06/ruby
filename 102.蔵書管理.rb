c5624353a1725534373ce8dd585457963adbcbe7
junnnnn06 <[junnnnn06@icloud.com]>
2017-09-01 16:18:02 +0900

    テンプレート生成

---
+++ 102.蔵書管理.rb
@@ -0,0 +1,12 @@
+# printf( "2000000 + 2 = %04d\n", 2000000 + 2 )
+# printf( " 2.54 * 14.3 = %4.2f\n", 2.54 * 14.3)
+
+require 'date'
+
+# day = Date.today
+# puts day.to_s
+
+today = Date.new(2009,9,7)
+tomorrow = Date.new(2009,9,8)
+
+puts tomorrow.to_s
